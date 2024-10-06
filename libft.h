51c96d2518e5ed7d1620bbbf507b816fc4c22f61
Adrian Gallego Martin <adrgalle@c2r16s6.42madrid.com>
2024-10-06 18:32:56 +0200

    Añadidas funciones hasta ft_memchr.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: adrgalle <adrgalle@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 18:32:03 by adrgalle          #+#    #+#             */
-/*   Updated: 2024/09/22 13:35:31 by adrgalle         ###   ########.fr       */
+/*   Updated: 2024/10/06 13:15:28 by adrgalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,16 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 size_t	ft_strlen(const char *c);
 void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strrchr(const char *str, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t size);
+void	*ft_memchr(const void *s, int c, size_t n);
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: adrgalle <adrgalle@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 18:56:38 by adrgalle          #+#    #+#             */
-/*   Updated: 2024/09/22 12:58:21 by adrgalle         ###   ########.fr       */
+/*   Updated: 2024/10/06 13:14:54 by adrgalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,16 @@ int	main(void)
 	int	isascii;
 	int	isprint;
 	int	strlen;
-	char str[50] = "Prueba memset"; // Cadena original
+	char str[50] = "Prueba memset";
+	char str2[50] = "prueba bzero";
+	char str3[50] = "prueba memcopy";
+	char str4[50];
+	char str5[50];
+	char str6[50];
+	char str7[50] = "Concatenar ";
+	int c = 'o';
+	const char *str8 = "Hola Mundo";
+	const char *str9 = "Hola Munda";
 
 	isalpha = ft_isalpha('b');
 	isdigit = ft_isdigit('b');
@@ -29,6 +38,15 @@ int	main(void)
 	isprint = ft_isprint('b');
 	strlen = ft_strlen("bajo");
 	ft_memset(str, '*', 13);
+	ft_bzero(str2, 8);
+	ft_memcpy(str4, str3, 8);
+	ft_memcpy(str5, str3, 3);
+	ft_strlcpy(str6, str3, 6);
+	ft_strlcat(str7, str3, 30);
+	char	*result = ft_strchr("String de prueba", c);
+	char	*result2 = ft_strrchr(str8, c);
+	char	*result3 = memchr("String para memchr", c, 18); 
+	
 	printf("El resultado de ft_isalpha para 'b' es %i", isalpha);
 	printf("\n");
 	printf("El resultado de ft_isdigit para 'b' es %i", isdigit);
@@ -42,6 +60,42 @@ int	main(void)
 	printf("El resultado de ft_strlen para 'bajo' es %i", strlen);
 	printf("\n");
 	printf("El resultado de ft_memset para 'Prueba memset' es %s", str);
-	
+        printf("\n");
+        printf("El resultado de ft_bzero para 'Prueba bzero' es %s", str2);
+	printf("\n");
+        printf("El resultado de f_memcopy para 'Prueba memcopy' con 8 caracteres es %s", str4);
+	printf("\n");
+        printf("El resultado de f_memmove para 'Prueba memcopy' con 3 caracteres es %s", str5);
+        printf("\n");
+        printf("El resultado de f_memmove para 'Prueba memcopy' con 6 de size es %s", str6);
+        printf("\n");
+        printf("El resultado de ft_strlcat para 'Prueba memcopy' + 'Concatenar ' es %s", str7);
+        printf("\n");
+        printf("El resultado de ft_toupper para 'v' es %c", ft_toupper('v'));
+        printf("\n");
+        printf("El resultado de ft_tolower para 'A' es %c", ft_tolower('A'));
+        printf("\n");
+ 
+        // Comprobación de ft_strchr
+	if (result != NULL) 
+        	printf("El resultado de ft_strchr para '%c' es '%c'\n", c, *result);
+   	else
+        	printf("Ft_strchr: El carácter '%c' no fue encontrado en la cadena 'String de prueba'.\n", c);
+	printf("\n");
+	// Comprobación de ft_strrchr
+	if (result2 != NULL)
+		printf("La última ocurrencia de '%c' en \"%s\" es en: %s\n", c, str8, result2);
+	else 
+		printf("El carácter '%c' no se encontró en \"%s\".\n", c, str8);
+		
+        printf("\n");
+        printf("El resultado de ft_strncmp para 'Hola mundo' y 'Hola munda' comparando 6 caracteres es %i", ft_strncmp(str8,str9,6));
+        printf("\n");
+        // Comprobación de ft_strrchr
+	if (result3 != NULL)
+		printf("El resultado de ft_memchr para la frase String para memchr y la letra '%c' es '%c'\n", c, *result);
+	else 
+		printf("En la frase 'String para memchr' no se encontro la letra o");
+        
 	return (0);
 }
